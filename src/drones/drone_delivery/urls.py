93bd070cd7f367e5e9dd269b6c1bde99ff3406a5
josueisaihs <josueisaihs@gmail.com>
2022-01-10 21:36:33 -0500

    (feature) CRUD actions created for the Drone model

--- src/drones/drone_delivery/urls.py
+++ src/drones/drone_delivery/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import (DroneDetailView, DronesListView)
+from .views import (DroneDetailView, DronesListCreateApiView, DroneDetailApiView)
 
 urlpatterns = [
     path("drone/<slug:slug>/detail/", DroneDetailView.as_view(), name="drone-detail"),
 
     # API
-    path("api/drone/list/", DronesListView.as_view(), name="api-drone-list")
+    path("api/drone/list/", DronesListCreateApiView.as_view(), name="api-drone-list"),
+    path("api/drone/<slug:slug>/detail/", DroneDetailApiView.as_view(), name="api-drone-detail")
 ]
--- src/drones/drone_delivery/views.py
+++ src/drones/drone_delivery/views.py
@@ -2,7 +2,7 @@
 from django.views.generic import DetailView
 from django_filters.filterset import filterset_factory
 
-from rest_framework.generics import (ListAPIView,)
+from rest_framework.generics import (ListCreateAPIView, RetrieveUpdateDestroyAPIView)
 from rest_framework.filters import (OrderingFilter)
 from django_filters.rest_framework import (DjangoFilterBackend)
 
@@ -13,10 +13,15 @@ from .serializers import (DroneSerializer)
 from .filters import (DroneFilter)
 
 class DroneDetailView(DetailView):
+    # Only for debug
     model = Drone
 
+class DroneDetailApiView(RetrieveUpdateDestroyAPIView):
+    serializer_class = DroneSerializer
+    queryset = Drone.objects.all()
+    lookup_field = "slug"
 
-class DronesListView(ListAPIView):
+class DronesListCreateApiView(ListCreateAPIView):
     serializer_class = DroneSerializer
     queryset = Drone.objects.all()
     filter_backends = [DjangoFilterBackend, OrderingFilter]
